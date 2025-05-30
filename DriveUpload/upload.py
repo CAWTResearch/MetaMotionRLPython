b21396ba1ee306fb49ac2c5abb649c2efd55626f
Yahid1 <yahid.diaz@upr.edu>
2025-05-30 16:01:58 -0400

    Changing names

---
+++ DriveUpload/upload.py
@@ -0,0 +1,29 @@
+#pip install google-api-python-client
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+SCOPES = ['https://www.googleapis.com/auth/drive']
+SERVICE_ACCOUNT_FILE = 'service_account.json'
+PARENT_FOLDER_ID = "16DwleohuGulUcZ0tjZHkda0lFqkJ6e7l"
+
+def authenticate():
+    creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    return creds
+
+def upload_photo(file_path):
+    creds = authenticate()
+    service = build('drive', 'v3', credentials=creds)
+
+    file_metadata = {
+        'name' : "NewData",
+        'parents' : [PARENT_FOLDER_ID]
+    }
+
+    file = service.files().create(
+        body=file_metadata,
+        media_body=file_path
+    ).execute()
+
+upload_photo("NewData.csv")
+print("File uploaded successfully.")
+print('See Google Drive for the uploaded file.')
