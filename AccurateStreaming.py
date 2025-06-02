ee49436621ee96b06a80b7a110ab7e27bcd0387b
CAWTResearch <etechlab@uprm.edu>
2025-06-02 11:00:08 -0400

    Dongle Update

--- AccurateStreaming.py
+++ AccurateStreaming.py
@@ -10,9 +10,8 @@ import subprocess, time, datetime, os, csv, signal, sys
 # Sensor y dongle MACs
 device_macs = ["C4:65:87:1A:13:0B","D5:42:DD:AC:BE:E1", "E6:AC:5E:B8:4C:D9", "F0:3D:E7:ED:F6:F7", "E6:4F:B9:D7:18:7C", "CE:5A:39:E6:8F:B3"]
 # dongle_macs = ["3C:0A:F3:10:17:F0"]
-dongle_macs = ["D8:3A:DD:EA:0C:EF","00:E0:5C:48:03:93","00:E0:5C:48:01:63"]
+dongle_macs = ["D8:3A:DD:EA:0C:EF","00:E0:5C:48:06:BD","00:E0:5C:48:01:63"]
 states = []
-
 # Asegura desconexión previa
 
 def force_disconnect_sensors():
