e3633cfcb9425d7d656b5f9277dee62f197f0bbc
Yahid1 <yahid.diaz@upr.edu>
2025-06-05 15:40:31 -0400

    Getting rid the new stuff

--- AccurateStreaming.py
+++ AccurateStreaming.py
@@ -139,9 +139,9 @@ def connect_sensors(devices, dongles, retries=5):
 def configure_and_subscribe_sensors(states):
     for st in states:
         b = st.device.board
-        libmetawear.mbl_mw_settings_set_connection_parameters(
-            b , 750.0, 750.0, 128, 16384
-        )
+        # libmetawear.mbl_mw_settings_set_connection_parameters(
+        #     b , 750.0, 750.0, 128, 16384
+        # )
 
         # ACC: set ODR and range
         libmetawear.mbl_mw_acc_bmi270_set_odr(b, AccBmi270Odr._50Hz)
