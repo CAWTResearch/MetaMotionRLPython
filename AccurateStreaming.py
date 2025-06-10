ec5e1bf4f93547b8cb5994a39f23b4ae0cb90851
Yahid1 <yahid.diaz@upr.edu>
2025-06-10 11:44:08 -0400

    Old sensors Test

--- AccurateStreaming.py
+++ AccurateStreaming.py
@@ -10,15 +10,18 @@ import subprocess, time, datetime, os, csv, signal, sys
 # Sensor y dongle MACs
 # device_macs = ["F0:3D:E7:ED:F6:F7", "CE:5A:39:E6:8F:B3", "E6:AC:5E:B8:4C:D9",'F8:DC:C7:F1:48:7A',"E6:4F:B9:D7:18:7C"]
 # device_macs = ['F8:DC:C7:F1:48:7A',"E6:4F:B9:D7:18:7C"]
-device_macs = ["F0:3D:E7:ED:F6:F7", "CE:5A:39:E6:8F:B3", "E6:AC:5E:B8:4C:D9"]
-# dongle_macs = ['00:E0:5C:48:00:2F','00:E0:5C:48:01:63', 'D8:3A:DD:EA:0C:EF', '00:E0:5C:48:03:93', '00:E0:5C:48:06:BD']
-dongle_macs = ['D8:3A:DD:EA:0C:EF', '00:E0:5C:48:03:93', '00:E0:5C:48:06:BD']
+# device_macs = ["F0:3D:E7:ED:F6:F7", "CE:5A:39:E6:8F:B3", "E6:AC:5E:B8:4C:D9"]
+dongle_macs = ['00:E0:5C:48:00:2F','00:E0:5C:48:01:63', 'D8:3A:DD:EA:0C:EF', '00:E0:5C:48:03:93', '00:E0:5C:48:06:BD']
+# dongle_macs = ['D8:3A:DD:EA:0C:EF', '00:E0:5C:48:03:93', '00:E0:5C:48:06:BD']
 # dongle_macs = ["00:E0:5C:48:01:34", "00:E0:5C:48:0B:98"]
 
 states = []
 
 # Asegura desconexión previa
 STREAM_DURATION = 60
+
+# Old Sensor MACs for reference
+device_macs = ["EE:1B:72:FA:BF:E8","FA:F1:20:99:CB:B4","CE:94:48:FE:5D:C5","EC:57:2E:32:05:52", "F1:1E:E2:6F:1D:E1"]
 
 def force_disconnect_sensors():
     try:
