25ccb7e986f7ca73d948011c5491079407868d4e
CAWTResearch <etechlab@uprm.edu>
2025-06-30 14:41:05 -0400

    weird problem?

--- DriveUpload/upload.py
+++ DriveUpload/upload.py
@@ -43,6 +43,7 @@ def upload_file(service, file_path, folder_id):
     }
     service.files().create(
         body=file_metadata,
+        supportsAllDrives=True,
         media_body=file_path
     ).execute()
 
