a20a6b3acbb8cefd1680f1cf4f7bfe5ef27cecca
CAWTResearch <etechlab@uprm.edu>
2025-08-15 12:02:48 -0400

    debug2

--- server.py
+++ server.py
@@ -171,6 +171,8 @@ def plan_from_mapping(mapping: Dict[str, str]) -> Dict[str, List[Tuple[str, str]
     quats   = [(pos, mac) for (pos, mac) in selected if ROLE_BY_POSITION.get(pos) == 'quat']
     device_macs = [mac for (_, mac) in normals] + [mac for (_, mac) in quats]
     print(device_macs)
+    print(normals)
+    print(quats)
     return {"normals": normals, "quats": quats, "device_macs": device_macs}
 
 def preprocess_data(buffer, scaler):
